cf7e09775a80123b6e9563da6d018417f88a26a2
hakzzang <mbh930312@naver.com>
2023-07-09 13:55:23 +0900

    [테스트] 자동차가 몇 번 이동하는 지에 대한 테스트 코드 추가

--- src/test/kotlin/CarTrackTest.kt
+++ src/test/kotlin/CarTrackTest.kt
@@ -10,7 +10,7 @@ class CarTrackTest {
         // given
         val carTrack = CarTrack()
         // when
-        carTrack.init("BTS,봉준호,손흥민,제이팍,렛츠고")
+        carTrack.init("BTS,봉준호,손흥민,제이팍,렛츠고", racingStepCount = 1)
         // then
         Assertions.assertEquals(carTrack.cars.size == 5, true)
     }
@@ -21,8 +21,30 @@ class CarTrackTest {
         // given
         val carTrack = CarTrack()
         // when
-        carTrack.init("BTS,봉준호,손흥민,제이팍,렛츠고")
+        carTrack.init(inputCarName = "BTS,봉준호,손흥민,제이팍,렛츠고", racingStepCount = 1)
         // then
         Assertions.assertEquals(carTrack.cars.size == 4, false)
+    }
+
+    @Test
+    @DisplayName("n대의 자동차에 대한 m번 이동하는 것에 대한 성공 테스트")
+    fun testInputPlayLoopThenTrue() {
+        // given
+        val carTrack = CarTrack()
+        // when
+        carTrack.init("BTS,봉준호,손흥민,제이팍,렛츠고", racingStepCount = 1)
+        // then
+        Assertions.assertEquals(carTrack.racingStepCount == 1, true)
+    }
+
+    @Test
+    @DisplayName("n대의 자동차에 대한 m번 이동하는 것에 대한 실패 테스트")
+    fun testInputPlayLoopThenFalse() {
+        // given
+        val carTrack = CarTrack()
+        // when
+        carTrack.init("BTS,봉준호,손흥민,제이팍,렛츠고", racingStepCount = 1)
+        // then
+        Assertions.assertEquals(carTrack.racingStepCount == 2, false)
     }
 }
