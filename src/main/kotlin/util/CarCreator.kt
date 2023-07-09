a955749452aac9507ed40d1edc5ffb9afc04a1cb
hakzzang <mbh930312@naver.com>
2023-07-09 13:35:49 +0900

    [기능] 자동차 이름을 입력하는 기능 추가

--- src/main/kotlin/util/CarCreator.kt
+++ src/main/kotlin/util/CarCreator.kt
@@ -6,4 +6,8 @@ class CarCreator {
     fun create(name: String): Car {
         return Car(name)
     }
+
+    fun createCars(inputCarName: String): List<Car> {
+        return inputCarName.split(",").map { name -> create(name) }.toList()
+    }
 }
--- src/main/kotlin/util/CarNameChecker.kt
+++ src/main/kotlin/util/CarNameChecker.kt
@@ -1,10 +1,19 @@
 package util
 
 import model.Car
+import java.security.InvalidParameterException
 
 class CarNameChecker {
     fun isValidate(car: Car): Boolean {
         if (car.name.isBlank() || car.name.length > 5) return false
         return true
+    }
+
+    fun isValidate(cars: List<Car>): Boolean {
+        val isValidated = cars.all { car -> isValidate(car) }
+        if (!isValidated) {
+            throw InvalidParameterException("자동차 이름이 규격에 맞지 않습니다.")
+        }
+        return isValidated
     }
 }
