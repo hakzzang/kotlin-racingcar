141c0064827689b5765e7806672a8966b58f5636
hakzzang <mbh930312@naver.com>
2023-07-09 14:20:28 +0900

    [테스트] 자동차의 상태를 화면에 출력하는 테스트 코드 추가

--- src/test/kotlin/CarTrackTest.kt
+++ src/test/kotlin/CarTrackTest.kt
@@ -80,7 +80,29 @@ class CarTrackTest {
         )
         carTrack.startRacing()
         // then
-        carDashboard.toTemplateMessage(carTrack.cars[0])
         Assertions.assertEquals(carDashboard.toTemplateMessage(carTrack.cars[0]), "BTS 자동차는 1만큼 이동했습니다.")
+    }
+
+    @Test
+    @DisplayName("전진하는 자동차를 출력할 때 자동차 이름을 같이 출력 테스트")
+    fun testPrintCarRacingResultThenFalse() {
+        // given
+        val carTrack = CarTrack()
+        // when
+        carTrack.init(
+            inputCarName = "BTS,봉준호,손흥민,제이팍,렛츠고",
+            racingStepCount = 5,
+            engine = FakeEngine(4)
+        )
+        carTrack.startRacing()
+
+        // then
+        Assertions.assertEquals(carTrack.printCarRacingResult(), listOf(
+            "BTS 자동차는 5만큼 이동했습니다.",
+            "봉준호 자동차는 5만큼 이동했습니다.",
+            "손흥민 자동차는 5만큼 이동했습니다.",
+            "제이팍 자동차는 5만큼 이동했습니다.",
+            "렛츠고 자동차는 5만큼 이동했습니다."
+        ))
     }
 }
